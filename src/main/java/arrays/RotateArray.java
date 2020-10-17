ca56ed9c0bfc47d30ad2f735410117e7febde76d
Sahil Suri <sahilsuridev@gmail.com>
2020-10-17 11:10:23 +1100

    Rotate Array

--- src/main/java/arrays/RotateArray.java
+++ src/main/java/arrays/RotateArray.java
@@ -4,6 +4,9 @@ public class RotateArray {
     public static void main(String[] args) {
         int [] arr = {1, 3, 7, 9, 22, 23, 25};
         int d = 3;
+
+        int[] num = {1,2,3,4,5,6,7 };
+        rotate(num, 3);
         // final output should be 9, 22, 23, 25, 1, 3, 7
 
         reverseArray(arr, 0, d-1);
@@ -45,6 +48,30 @@ public class RotateArray {
         System.out.println("===== rotate array ====");
         for (int a:arr) {
             System.out.print(a + " ");
+        }
+    }
+
+    /*
+    Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+     */
+
+    public static void rotate(int[] nums, int k) {
+        if (k > nums.length) {
+            k = k % nums.length;
+        }
+        int outerLoop = 0;
+        while(outerLoop < k) {
+            int temp = nums[nums.length -1];
+            for(int i = nums.length -1; i >= 1; i--) {
+                nums[i] = nums[i -1];
+            }
+            nums[0] = temp;
+            outerLoop ++;
         }
     }
 }
