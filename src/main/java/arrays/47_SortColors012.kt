5df043d75815d9ca957648c13490d65b6f48b0af
Sahil Suri <sahilsuridev@gmail.com>
2020-10-18 15:10:35 +1100

    Dutch National Flag algo for Sorting 0,1,2

--- src/main/java/arrays/47_SortColors012.kt
+++ src/main/java/arrays/47_SortColors012.kt
@@ -1,2 +1,41 @@
 package arrays
 
+class SortColor {
+    // Dutch National Flag algo
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val arr = intArrayOf(2,0,2,1,1,0)
+            sortColors(arr)
+            println("** output "+ arr.toList())
+        }
+    }
+}
+
+fun sortColors(nums: IntArray): Unit {
+    if(nums.size <=1) return
+    var low = 0
+    var mid = 0
+    var high = nums.size - 1
+    while (mid <= high) {
+        when(nums[mid]) {
+            0 -> {
+                val temp = nums[mid]
+                nums[mid] = nums[low]
+                nums[low] = temp
+                low ++
+                mid ++
+            }
+            1 -> {
+                mid ++
+            }
+            2-> {
+                val temp = nums[mid]
+                nums[mid] = nums[high]
+                nums[high] = temp
+                high --
+            }
+        }
+    }
+}
